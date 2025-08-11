b456231548491e36f67e23026b27acbec0900740
bulviakas <misteridiot2004@gmail.com>
2025-08-11 16:38:37 +0300

    Shifted all of the constants from main.py to config.py

--- main.py
+++ main.py
@@ -2,69 +2,9 @@ import os
 if "DISPLAY" not in os.environ:   # needed when starting from cron/rc.local
     os.environ["DISPLAY"] = ":0"
 import tkinter as tk
-from pathlib import Path
 from utils import svg_to_coloured_photo, svg_to_photo
 from ui import VideoPlayer
-
-# ASSET PATHS
-RESTART_BUTTON_PATH = Path("assets/restart_btn.svg")
-SUBMIT_BUTTON_PATH = Path("assets/submit_btn.svg")
-START_BLOCK_PATH = Path("assets/start_block.svg")
-CMD_BLOCK_PATH = Path("assets/game_blocks.svg")
-BLOCK_TEMPLATE_PATH = Path("assets/block_template.svg")
-CMD_BLOCK_PATH = Path("assets/cmd_block.svg")
-TT_ICON_PATH = Path("assets/tutorial_icon_thin.svg")
-HOME_ICON_PATH = Path("assets/home_icon_thin.svg")
-LNG_ICON_PATH = Path("assets/language_icon_thin.svg")
-START_BTN_PATH = Path("assets/start_btn.svg")
-CONTEXT_VIDEO_PATH = Path("assets/Fish-spinning.mp4")
-INSTRUCTIONS_VIDEO_PATH = Path("assets/roach.gif")
-
-# CONSTANTS
-OVERLAP_FRAC        = 0.2
-CMD_BAR_HEIGHT_FRAC = 1.7
-TOP_PAD_FRAC        = 0.19
-MID_PAD_FRAC        = 0.05   # vertical gap between cmd bar & button
-BOT_PAD_FRAC        = 0.1
-SIDE_GAP_FRAC       = 0.02   # side padding for menu
-GAP_BETWEEN_BTNS    = 3
-BLOCK_SIZE_COEF     = 0.02
-CMD_SIDE_PAD        = 32
-CMD_H_PAD           = 16
-MENU_TOP_FRAC       = 0.08
-
-# TODO: import better fonts
-
-# Good fonts: Cascadia Code SemiBold, Segoe UI Black
-MAIN_FONT = "Cascadia Code SemiBold"
-
-BLOCK_COLOURS = [
-    "#e74c3c", "#f39c12", "#27ae60", "#8e44ad", "#96d5ff",
-    "#ff5f7f", "#f1c40f", "#2980b9"   # 8 pieces
-]
-
-BLOCK_LABELS = [
-    "TESTAVIMO\nPOZICIJA", "SILPNAS\nPAKRATYMAS", "STIPRUS\nPAKRATYMAS", "ŠALDYTUVO\nPOZICIJA",
-    "SKENAVIMO\nPOZICIJA", "TRUMPA\nPAUZĖ", "ILGA\nPAUZĖ", "GALUTINĖ\nPOZICIJA"
-]
-
-THE_CORRECT_SEQUENCE = [
-    "ŠALDYTUVO\nPOZICIJA", "TESTAVIMO\nPOZICIJA", "STIPRUS\nPAKRATYMAS", "SKENAVIMO\nPOZICIJA",
-    "ILGA\nPAUZĖ", "TESTAVIMO\nPOZICIJA", "GALUTINĖ\nPOZICIJA", None
-]
-
-CONTEXT_VIDEO_SIZE = [480, 854] # Change when importing the actual video
-INSTRUCTIONS_VIDEO_SIZE = [360, 480]
-
-CONTEXT_TEXT = (
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus at sapien ac egestas. "
-    "Morbi quis tellus ut mauris efficitur pellentesque ac luctus nisl. Pellentesque pharetra sapien dui. "
-    "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. "
-    "Vivamus a tortor nec elit auctor sagittis eu at risus. Nulla ut ex eu justo luctus blandit ac non ipsum. "
-    "Mauris sollicitudin fringilla eros, ac vulputate orci sagittis non. "
-    "Donec a tortor vestibulum, blandit enim quis, convallis dolor. Curabitur laoreet justo quis rutrum pellentesque. "
-    "Curabitur nisi ex, ornare eu blandit at, pulvinar eu eros. Proin sollicitudin massa sed nibh sollicitudin bibendum. "
-)
+from config import *
 
 class PuzzleApp:
     def __init__(self):
