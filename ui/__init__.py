8f10f031169cc678571a35492dce9e67c2cfca06
bulviakas <misteridiot2004@gmail.com>
2025-08-11 18:37:49 +0300

    Seperated the page setup from the main file

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,5 +1,12 @@
 from .video_player import VideoPlayer
 from .block import Block
 from .command_line import CommandLine
+from .pages import setup_start_page, setup_instructions_page, setup_context_page, setup_game_page
 
-__all__ = ["VideoPlayer", "Block", "CommandLine"]
+__all__ = [
+    "VideoPlayer", "Block", "CommandLine", 
+    "setup_game_page", 
+    "setup_context_page", 
+    "setup_instructions_page",
+    "setup_start_page"
+    ]
