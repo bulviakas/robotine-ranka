71457c0f4b0bd07aa7f694bc36eeccac362d347d
bulviakas <misteridiot2004@gmail.com>
2025-08-12 15:13:24 +0300

    Added the create_button function to clean up the code

--- ui/pages.py
+++ ui/pages.py
@@ -14,10 +14,16 @@ def setup_start_page(self):
 
     start_tag = 'start'
     start_img = load_svg_img(self, START_BTN_PATH, (self.self_w / 2, self.self_h / 5), BLOCK_COLOURS[2])
-    start_btn = canvas.create_image(int(self.self_w * 0.5), int(self.self_h * 0.6), image=start_img, anchor='center', tags=start_tag)
-    canvas.tag_bind(start_tag, "<Button-1>", lambda e: self.show_page(self.context_page))
 
-    canvas.create_text(int(self.self_w * 0.5), int(self.self_h * 0.595), text="ŽAISTI", font=(MAIN_FONT, int(48 * (self.self_h / 5 / 165)), 'bold'), fill='black', tags=start_tag)
+    create_button(
+        self, canvas, 
+        img=start_img, 
+        x=int(self.self_w * 0.5), y=int(self.self_h * 0.6), 
+        text="ŽAISTI", 
+        font_size=int(48 * (self.self_h / 5 / 165)), 
+        tag=start_tag, 
+        command=lambda e: self.show_page(self.context_page)
+        )
 
 def setup_context_page(self):
     canvas = tk.Canvas(self.context_page, width=self.self_w, height=self.self_h, bg='black', highlightthickness=0)
@@ -29,10 +35,18 @@ def setup_context_page(self):
     # NEXT Button
     self.next_tag = 'next'
     next_img = load_svg_img(self, CMD_BLOCK_PATH, (1.25*self.piece_w, 1.25*self.piece_h), BLOCK_COLOURS[6])
-    self.next_btn = canvas.create_image(int(self.self_w * 0.9), int(self.self_h * 0.9), image=next_img, anchor='center', tags=self.next_tag)
-    canvas.create_text(int(self.self_w * 0.9) + 7, int(self.self_h * 0.9), 
-                                    text="TOLIAU", font=(MAIN_FONT, int(18 * BLOCK_SIZE_COEF), 'bold'), fill='black', tags=self.next_tag)
-    canvas.tag_bind(self.next_tag, "<Button-1>", lambda e: self.show_page(self.instructions_page))
+
+    create_button(
+        self, canvas,
+        img=next_img,
+        x=int(self.self_w * 0.9),
+        y=int(self.self_h * 0.9),
+        text="TOLIAU",
+        font_size=int(18 * BLOCK_SIZE_COEF),
+        tag=self.next_tag,
+        command=lambda e: self.show_page(self.instructions_page),
+        text_offset_x=7
+    )
 
     # Video block
     video_h = self.self_h * 0.75
@@ -54,18 +68,34 @@ def setup_instructions_page(self):
     # BACK Button
     self.back_tag = 'back'
     back_img = load_svg_img(self, CMD_BLOCK_PATH, (1.25*self.piece_w, 1.25*self.piece_h), BLOCK_COLOURS[5], mirror=True)
-    back_btn = canvas.create_image(int(self.self_w * 0.1), int(self.self_h * 0.9), image=back_img, tags=self.back_tag)
-    canvas.create_text(int(self.self_w * 0.1 - 7), int(self.self_h * 0.9), 
-                                    text="ATGAL", font=(MAIN_FONT, int(18 * BLOCK_SIZE_COEF), 'bold'), fill='black', tags=self.back_tag)
-    canvas.tag_bind(self.back_tag, "<Button-1>", lambda e: self.show_page(self.context_page))
+
+    create_button(
+        self, canvas,
+        img=back_img,
+        x=int(self.self_w * 0.1), 
+        y=int(self.self_h * 0.9),
+        text="ATGAL",
+        font_size=int(18 * BLOCK_SIZE_COEF),
+        tag=self.back_tag,
+        command=lambda e: self.show_page(self.context_page),
+        text_offset_x=-7
+    )
 
     # PLAY Button
     self.play_tag = 'play'
     play_img = load_svg_img(self, CMD_BLOCK_PATH, (1.25*self.piece_w, 1.25*self.piece_h), BLOCK_COLOURS[6])
-    self.next_btn = canvas.create_image(int(self.self_w * 0.9), int(self.self_h * 0.9), image=play_img, anchor='center', tags=self.play_tag)
-    canvas.create_text(int(self.self_w * 0.9) + 7, int(self.self_h * 0.9), 
-                                    text="TOLIAU", font=(MAIN_FONT, int(18 * BLOCK_SIZE_COEF), 'bold'), fill='black', tags=self.play_tag)
-    canvas.tag_bind(self.play_tag, "<Button-1>", lambda e: self.show_page(self.game_page))
+
+    create_button(
+        self, canvas,
+        img=play_img,
+        x=int(self.self_w * 0.9),
+        y=int(self.self_h * 0.9),
+        text="TOLIAU",
+        font_size=int(18 * BLOCK_SIZE_COEF),
+        tag=self.play_tag,
+        command=lambda e: self.show_page(self.game_page),
+        text_offset_x=7
+    )
 
     # FIXME change the relative placement so that the video with the text would be centered
 
@@ -120,10 +150,18 @@ def setup_game_page(self):
     self.clear_tag = 'clear'
     btn_left_1 = self.self_w//2 - btn_w - GAP_BETWEEN_BTNS
     clear_img = load_svg_img(self, RESTART_BUTTON_PATH, (btn_w, btn_h), 'white')
-    self.clear_btn = self.canvas.create_image(btn_left_1, btn_top, image=clear_img, anchor='nw', tags=self.clear_tag)
-    self.canvas.create_text(btn_left_1 + btn_w * 0.55, btn_top + btn_h//2 - 3, 
-                            text="IŠVALYTI", font=(MAIN_FONT, int(18 * (btn_h / 51)), 'bold'), fill='black', tags=self.clear_tag)
-    self.canvas.tag_bind(self.clear_tag, "<Button-1>", self.cmd.clear)
+
+    create_button(
+        self, self.canvas,
+        img=clear_img,
+        x=btn_left_1 + btn_w / 2,
+        y=btn_top + btn_h / 2,
+        text="IŠVALYTI",
+        font_size=int(18 * (btn_h / 51)),
+        tag=self.clear_tag,
+        command=self.cmd.clear,
+        text_offset_x=btn_w * 0.05,
+    )
 
     # SUBMIT button
     self.submit_tag = 'submit'
@@ -133,6 +171,18 @@ def setup_game_page(self):
     self.canvas.create_text(btn_left_2 + btn_w * 0.45, btn_top + btn_h//2 - 3, 
                             text="PALEISTI", font=(MAIN_FONT, int(18 * (btn_h / 51)), 'bold'), fill='black', tags=self.submit_tag)
     self.canvas.tag_bind(self.submit_tag, "<Button-1>", self.cmd.submit)
+
+    create_button(
+        self, self.canvas,
+        img=submit_img,
+        x=btn_left_2 + btn_w * 0.5,
+        y=btn_top + btn_h//2,
+        text="PALEISTI",
+        font_size=int(18 * (btn_h / 51)),
+        tag=self.submit_tag,
+        command=self.cmd.submit,
+        text_offset_x=-btn_w * 0.05
+    )
 
     # ICONS ---
     icon_size = self.self_h * TOP_PAD_FRAC // 2.75
@@ -182,3 +232,8 @@ def setup_game_page(self):
         row, col = divmod(idx, 4)
         label = BLOCK_LABELS[idx]
         Block(self, self.cmd, colour, col_x[col], row_centres[row], template=True, text=label)
+
+def create_button(self, canvas, img, x, y, text, font_size, tag, command, text_offset_x=0, anchor="center"):
+    canvas.create_image(x, y, image=img, tags=tag, anchor=anchor)
+    canvas.create_text(x + text_offset_x, y, text=text, font=(MAIN_FONT, font_size, 'bold'), fill='black', tags=tag)
+    canvas.tag_bind(tag, "<Button-1>", command)
