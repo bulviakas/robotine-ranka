2c2a9cda6ac759bfb8799fe4fbe897acd04ed491
bulviakas <misteridiot2004@gmail.com>
2025-11-26 17:26:27 +0200

    Made the SequenceExecutor class'

--- sequence_executor.py
+++ sequence_executor.py
@@ -4,44 +4,85 @@ from logger import get_logger
 from config import *
 logger = get_logger("Sequence Executor")
 
+class SequenceExecutor:
+    def __init__(self):
+        self.logger = get_logger("Sequence Executor")
 
-pins = [FRIDGE_POS_PIN, TEST_POS_PIN, STRONG_SHAKE_PIN, 
-    SCAN_POS_PIN, LONG_PAUSE_PIN, END_POS_PIN, 
-    WEAK_SHAKE_PIN, SHORT_PAUSE_PIN, ERROR_LED_PIN, PASS_LED_PIN]
+        self.pins = [
+            FRIDGE_POS_PIN,
+            TEST_POS_PIN,
+            STRONG_SHAKE_PIN,
+            SCAN_POS_PIN,
+            LONG_PAUSE_PIN,
+            END_POS_PIN,
+            WEAK_SHAKE_PIN,
+            SHORT_PAUSE_PIN,
+            ERROR_LED_PIN,
+            PASS_LED_PIN
+        ]
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
-for i in range(0, 10):
-    GPIO.setup(pins[i], GPIO.OUT)
+        for pin in self.pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.LOW)
 
-def right_sequence():
-    fridge_pos()
-    test_pos()
-    strong_shake()
-    scan_pos()
-    long_pos()
-    end_pos()
-    passed()
+    # Internal helper
+    def _run_action(self, pin, duration):
+        GPIO.output(pin, GPIO.HIGH)
+        sleep(duration)
+        GPIO.output(pin, GPIO.LOW)
 
-def fridge_pos(): run_action(FRIDGE_POS_PIN, 1)
-def test_pos(): run_action(TEST_POS_PIN, 1)
-def strong_shake(): run_action(STRONG_SHAKE_PIN, 3)
-def scan_pos(): run_action(SCAN_POS_PIN, 1)
-def long_pos(): run_action(LONG_PAUSE_PIN, 5)
-def end_pos(): run_action(END_POS_PIN, 1)
-def weak_shake(): run_action(WEAK_SHAKE_PIN, 3)
-def short_pause(): run_action(SHORT_PAUSE_PIN, 3)
-def passed(): run_action(PASS_LED_PIN, 5)
-def error():
-    for i in range(0,3):
-        run_action(ERROR_LED_PIN, 0.5)
-        sleep(0.5)
+    # Robot actions
+    def fridge_pos(self):
+        self._run_action(FRIDGE_POS_PIN, 1)
 
-def run_action(pin, duration):
-    GPIO.output(pin, GPIO.HIGH)
-    sleep(duration)
-    GPIO.output(pin, GPIO.LOW)
+    def test_pos(self):
+        self._run_action(TEST_POS_PIN, 1)
 
-right_sequence()
+    def strong_shake(self):
+        self._run_action(STRONG_SHAKE_PIN, 3)
+
+    def weak_shake(self):
+        self._run_action(WEAK_SHAKE_PIN, 3)
+
+    def scan_pos(self):
+        self._run_action(SCAN_POS_PIN, 1)
+
+    def long_pause(self):
+        self._run_action(LONG_PAUSE_PIN, 5)
+
+    def short_pause(self):
+        self._run_action(SHORT_PAUSE_PIN, 3)
+
+    def end_pos(self):
+        self._run_action(END_POS_PIN, 1)
+
+    def passed(self):
+        self._run_action(PASS_LED_PIN, 5)
+
+    def error(self):
+        for i in range(3):
+            self._run_action(ERROR_LED_PIN, 0.5)
+            sleep(0.5)
+
+    def right_sequence(self):
+        """Runs the full correct robot workflow."""
+        self.logger.info("Starting RIGHT sequence")
+
+        self.fridge_pos()
+        self.test_pos()
+        self.strong_shake()
+        self.scan_pos()
+        self.long_pause()
+        self.end_pos()
+        self.passed()
+
+        self.logger.info("RIGHT sequence completed")
+
+    def cleanup(self):
+        """Cleanup GPIO state."""
+        GPIO.cleanup()
+        self.logger.info("GPIO cleanup done")
 
