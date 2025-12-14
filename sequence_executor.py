732ff68c7b194d6578dc0a38a804425d8327a47d
bulviakas <misteridiot2004@gmail.com>
2025-12-14 11:50:40 +0200

    Adapted the pins to actually possible hardware actions

--- sequence_executor.py
+++ sequence_executor.py
@@ -4,18 +4,26 @@ from logger import get_logger
 from config import *
 logger = get_logger("Sequence Executor")
 
+# All possible hardware paths:
+# 1. Fridge - Home->fridge
+# 2. Test   - Fridge->test
+# 3. Shake  - Test->shake->test
+# 4. Scan   - Test->scan
+# 5. End    - Scan->home
+# 6. Err_f  - Fridge->home
+# 7. Err_t  - Test->home
+
 class SequenceExecutor:
     def __init__(self):
 
         self.pins = [
             FRIDGE_POS_PIN,
             TEST_POS_PIN,
-            STRONG_SHAKE_PIN,
+            SHAKE_PIN,
             SCAN_POS_PIN,
-            LONG_PAUSE_PIN,
+            ERR_FRIDGE_PIN,
             END_POS_PIN,
-            WEAK_SHAKE_PIN,
-            SHORT_PAUSE_PIN,
+            ERR_TEST_PIN,
             ERROR_LED_PIN,
             PASS_LED_PIN
         ]
@@ -31,7 +39,7 @@ class SequenceExecutor:
     def _run_action(self, pin, duration):
         GPIO.output(pin, GPIO.HIGH)
         sleep(duration)
-        GPIO.output(pin, GPIO.LOW)
+        GPIO.output(pin, GPIO.LOW) # FIXME Set before sleeping when switching to relays
 
     # Robot actions
     def fridge_pos(self):
@@ -41,27 +49,33 @@ class SequenceExecutor:
         self._run_action(TEST_POS_PIN, 1)
 
     def strong_shake(self):
-        self._run_action(STRONG_SHAKE_PIN, 3)
+        self._run_action(SHAKE_PIN, 3)
 
     def weak_shake(self):
-        self._run_action(WEAK_SHAKE_PIN, 3)
+        self._run_action(SHAKE_PIN, 3)
 
     def scan_pos(self):
         self._run_action(SCAN_POS_PIN, 1)
 
     def long_pause(self):
-        self._run_action(LONG_PAUSE_PIN, 3)
+        sleep(3)
 
     def short_pause(self):
-        self._run_action(SHORT_PAUSE_PIN, 3)
+        sleep(1.5)
 
     def end_pos(self):
         self._run_action(END_POS_PIN, 1)
 
     def passed(self):
         self._run_action(PASS_LED_PIN, 3)
+    
+    def error_fridge(self):
+        self._run_action(ERR_FRIDGE_PIN, 2)
 
-    def error(self):
+    def error_test(self):
+        self._run_action(ERR_TEST_PIN, 1)
+
+    def general_error(self):
         for i in range(3):
             self._run_action(ERROR_LED_PIN, 0.5)
             sleep(0.5)
@@ -79,11 +93,12 @@ class SequenceExecutor:
     def cleanup(self):
         """Cleanup GPIO state."""
         GPIO.cleanup()
-        self.logger.info("GPIO cleanup done")
+        logger.info("GPIO cleanup done")
 
     def execute(self, sequence):
         logger.info("Starting sequence execution...")
         for action in sequence:
+            logger.debug(f"Executing {action}")
             match action:
                 case "fridge_pos":
                     self.fridge_pos()
@@ -109,6 +124,11 @@ class SequenceExecutor:
                 case "short_pause":
                     self.short_pause()
                     continue
+                case _:
+                    logger.error(f"Unknown action: {action}")
+                    self.general_error()
+                    return
+
         logger.info("Sequence executed successfully")
         self.passed()
         return
