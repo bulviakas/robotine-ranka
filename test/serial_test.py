3b1cde46bd99668e8e53afb1e4b226bccb01efbf
bulviakas <misteridiot2004@gmail.com>
2026-03-15 17:43:45 +0200

    Fixed missing newlines

--- test/serial_test.py
+++ test/serial_test.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
     
     while True:
-        ser.write(("FRIDGE").encode('utf-8'))
+        ser.write(("FRIDGE\n").encode('utf-8'))
         time.sleep(3)
-        ser.write(("WEAK SHAKE").encode('utf-8'))
+        ser.write(("WEAK SHAKE\n").encode('utf-8'))
         time.sleep(3)
-        ser.write(("STRONG SHAKE").encode('utf-8'))
+        ser.write(("STRONG SHAKE\n").encode('utf-8'))
         time.sleep(3)
-        ser.write(("FINAL").encode('utf-8'))
+        ser.write(("FINAL\n").encode('utf-8'))
         time.sleep(3)
-        ser.write(("IDLE").encode('utf-8'))
+        ser.write(("IDLE\n").encode('utf-8'))
         time.sleep(3)
         
