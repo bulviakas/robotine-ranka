af08fdd0a5629009ad521063e9b475b90b3b0029
bulviakas <misteridiot2004@gmail.com>
2026-06-06 16:27:16 +0300

    Reverted Language Dropdown changes. Did not know it can get worse.

--- ui/language_dropdown.py
+++ ui/language_dropdown.py
@@ -5,89 +5,31 @@ from config import *
 
 class LanguageDropdown:
     def __init__(self, parent, languages, x, y, default="LT", command=None):
-        
-        self.command = command
-        self.languages = languages
-        self.current = default
-        self.x, self.y = x, y
-        self.parent = parent
-        self._popup = None
 
-        self.button = tk.Button(
-            parent,
-            text=default,
+        self.command = command
+        self.var = tk.StringVar(value=default)
+        self.x, self.y = x, y
+
+        self.dropdown = tk.OptionMenu(parent, self.var, *languages, command=self._on_select)
+        self.dropdown.config(
             bg=BLACK, fg=WHITE,
             font=(MAIN_FONT, 32),
             highlightthickness=2,
             highlightbackground=BLACK,
+            activebackground=BLACK,
+            activeforeground=WHITE,
+            indicatoron=0,
+            bd=0,
+            anchor="center"
+        )
+        self.dropdown["menu"].config(
+            bg=BLACK,
+            fg=WHITE,
+            font=(MAIN_FONT, 16),
             activebackground=WHITE,
             activeforeground=BLACK,
-            bd=0,
-            relief="flat",
-            anchor="center",
-            command=self._toggle_popup,
+            borderwidth=2,
+            relief="solid"
         )
-        self.button.place(x=self.x, y=self.y, anchor="center")
 
-    def _toggle_popup(self):
-        if self._popup and self._popup.winfo_exists():
-            self._close_popup()
-        else:
-            self._open_popup()
-
-    def _open_popup(self):
-        self._popup = tk.Toplevel(self.parent)
-        self._popup.overrideredirect(True)
-        self._popup.config(bg=BLACK)
-
-        bx = self.button.winfo_rootx()
-        by = self.button.winfo_rooty()
-        bh = self.button.winfo_height()
-
-        for i, lang in enumerate(self.languages):
-            btn = tk.Button(
-                self._popup,
-                text=lang,
-                bg=BLACK, fg=WHITE,
-                font=(MAIN_FONT, 26),
-                activebackground=WHITE,
-                activeforeground=BLACK,
-                bd=0,
-                relief="flat",
-                anchor="center",
-                width=4,
-                command=lambda l=lang: self._select(l),
-            )
-            btn.grid(row=i, column=0, sticky="ew", padx=2, pady=1)
-
-        self._popup.geometry(f"+{bx}+{by + bh + 2}")
-
-        # Close popup if user clicks outside
-        self._popup.bind("<FocusOut>", lambda _: self._close_popup())
-        self._popup.focus_set()
-
-    def _select(self, lang):
-        self.current = lang
-        self.button.config(text=lang)
-        self._close_popup()
-        if self.command:
-            self.command(lang)
-
-    def _close_popup(self):
-        if self._popup and self._popup.winfo_exists():
-            self._popup.destroy()
-        self._popup = None
-
-    def _on_select(self, lang):
-        if self.command:
-            self.command(lang)
-
-    def get(self):
-        """Return currently selected language."""
-        return self.current
-
-    def set(self, lang):
-        self.current = lang
-        self.button.config(text=lang)
-        if self.command:
-            self.command(lang)
+        self.dropdown.place(x=self.x, y=self.y, anchor="center")
